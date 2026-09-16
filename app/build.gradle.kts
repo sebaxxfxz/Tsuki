f97e58ecdcd964da5da0c02d427ec471b1cc2361
sebaxxfxz <sebasramigu21@gmail.com>
2026-09-15 20:01:36 -0500

    update version app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.tsuki"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0.0"
+        versionCode = 5
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
